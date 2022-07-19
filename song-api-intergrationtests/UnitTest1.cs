9194edc52f055196494ecfede43de90b73315f19
Jamey <jameyschaap06@gmail.com>
2022-07-19 09:39:45 +0200

    Setup test projects

---
+++ song-api-intergrationtests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace song_api_intergrationtests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ song-api-unittests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace song_api_unittests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
