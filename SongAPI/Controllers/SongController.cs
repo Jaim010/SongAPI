2df110fff0d64f4a609dce18f9d851cc1c6dd610
Jamey <jameyschaap06@gmail.com>
2022-07-23 23:48:36 +0200

    Added Swagger documentation

--- SongAPI/Controllers/SongController.cs
+++ SongAPI/Controllers/SongController.cs
@@ -4,8 +4,9 @@ using SongAPI.Services;
 
 namespace SongAPI.Controllers
 {
-  [Route("api/[controller]")]
   [ApiController]
+  [Route("api/[controller]")]
+  [Produces("application/json")]
   public class SongController : ControllerBase
   {
     private readonly ISongService _service;
@@ -13,8 +14,16 @@ namespace SongAPI.Controllers
     public SongController(ISongService service)
      => _service = service;
 
-    // GET: api/Song
+    // GET: api/Song/
+    /// <summary>
+    /// Gets all Songs.
+    /// </summary>
+    /// <returns>All Songs</returns>
+    /// <response code="200">Returns all songs</response>
+    /// <response code="404">If no songs exists</response>
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetSongs()
     {
       (IEnumerable<Song>? songs, Result result) = await _service.GetSongs();
@@ -26,7 +35,16 @@ namespace SongAPI.Controllers
     }
 
     // GET: api/Song/5
+    /// <summary>
+    /// Gets a Song by ID.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>A Song matching the given ID</returns>
+    /// <response code="200">Returns a song matching the given ID</response>
+    /// <response code="404">If no song matching the given ID exists</response>
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetSong(int id)
     {
       (Song? song, Result result) = await _service.GetSong(id);
@@ -39,7 +57,33 @@ namespace SongAPI.Controllers
 
     // PUT: api/Song/5
     // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    /// <summary>
+    /// Modifies an existing Song entity.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="song"></param>
+    /// <returns></returns>
+    /// <remarks>
+    /// Sample request:
+    /// 
+    ///   PUT /api/Song/1
+    ///   {
+    ///     "id": 1,
+    ///     "name": "Unstoppable",
+    ///     "artist": "For The Fallen Dreams",
+    ///     "imageurl": "https://picsum.photos/200/300"
+    ///   }
+    /// 
+    /// </remarks>
+    /// <response code="200">If the modification was succesfull</response>
+    /// <response code="400">If URL parameter 'id' and Song.Id do not match</response>
+    /// <response code="404">If no song matching the given ID exists</response>
+    /// <response code="500">If something went wrong while modifying the Song Entity</response>
     [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> PutSong(int id, Song song)
     {
       Result result = await _service.UpdateSong(id, song);
@@ -61,7 +105,27 @@ namespace SongAPI.Controllers
 
     // POST: api/Song
     // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    /// <summary>
+    /// Creates a new Song.
+    /// </summary>
+    /// <param name="song"></param>
+    /// <returns>A newly created Song</returns>
+    /// <remarks>
+    /// Sample request:
+    /// 
+    ///   POST /api/Song/
+    ///   {
+    ///     "name": "sTraNgeRs",
+    ///     "artist": "Bring Me The Horizon",
+    ///     "imageurl": "https://picsum.photos/200/300"
+    ///   }
+    /// 
+    /// </remarks>
+    /// <response code="201">Returns the newly created song</response>
+    /// <response code="400">If the song is null</response>
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> PostSong(Song song)
     {
       (Song? postedSong, Result result) = await _service.AddSong(song);
@@ -73,7 +137,16 @@ namespace SongAPI.Controllers
     }
 
     // DELETE: api/Song/5
+    /// <summary>
+    /// Deletes a specific Song.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    /// <response code="204">If the deletion was succesfull</response>
+    /// <response code="404">If no song matching the given ID exists</response>
     [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> DeleteSong(int id)
     {
       Result result = await _service.DeleteSong(id);
--- SongAPI/Models/Song.cs
+++ SongAPI/Models/Song.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace SongAPI.Models
 {
   public class Song
   {
 #pragma warning disable CS8618
+    [DefaultValue(0)]
     public int Id { get; set; }
+
+    [Required]
     public string Name { get; set; }
+
+    [Required]
     public string Artist { get; set; }
+
+    [Required]
     public string ImageUrl { get; set; }
 #pragma warning restore CS8618
   }
